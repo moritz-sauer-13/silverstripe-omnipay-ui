7db6a412b9537e1c5b860f1ed83aacfe11db8d84
Moritz Sauer <43135946+moritz-sauer-13@users.noreply.github.com>
2024-07-18 14:18:03 +0200

    Update PaymentAdmin.php

--- src/Admin/PaymentAdmin.php
+++ src/Admin/PaymentAdmin.php
@@ -20,7 +20,7 @@ class PaymentAdmin extends ModelAdmin
 
     private static $menu_title = 'Payments';
     private static $url_segment = 'payments';
-    private static $menu_icon = 'bummzack/silverstripe-omnipay-ui: client/dist/images/payment-admin.png';
+    private static $menu_icon = 'moritz-sauer-13/silverstripe-omnipay-ui: client/dist/images/payment-admin.png';
     private static $menu_priority = 1;
 
     public $showImportForm = false;
